b0eaed233189be8bc09bc552e9d94c8f3832c539
LeeHemingway <coherence0815@gmail.com>
2019-08-31 18:19:42 +0900

    refactoring

---
+++ generate_ngram_tree.py
@@ -0,0 +1,19 @@
+import os
+import wget
+import json
+from utils import process_wiki_data
+
+def generate_ngram_callback(title, text):
+    print(title)
+    print(text)
+
+if __name__ == '__main__':
+    INPUT_FILENAME = "jawiki-latest-pages-articles.xml.bz2"
+    JA_WIKI_LATEST_URL = "https://dumps.wikimedia.org/jawiki/latest/jawiki-latest-pages-articles.xml.bz2"
+    OUTPUT_FILENAME = "2gram.json"
+    ARTICLE_COUNT = 10
+    
+    if not os.path.isfile(INPUT_FILENAME):
+        wget.download(JA_WIKI_LATEST_URL)
+
+    process_wiki_data(INPUT_FILENAME, ARTICLE_COUNT, generate_ngram_callback)
--- utils.py
+++ utils.py
@@ -2,17 +2,14 @@
 # coding: utf-8
 
 import logging
-import os
 import time
 from multiprocessing import cpu_count
-import wget
-import json
 
 from mywiki import MyWikiCorpus
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-def process_wiki_to_ngram(input_filename, output_ngram_filename, depth):
+def process_wiki_data(input_filename, article_count, callback):
     start = time.time()
     intermediary_time = None
     count = 0
@@ -22,17 +19,16 @@ def process_wiki_to_ngram(input_filename, output_ngram_filename, depth):
     texts = wiki.get_texts()
 
     for i, article in enumerate(texts):
-        if i > 10:
+        if i > article_count:
             break
         
         title = article[1]
         text = article[0]  
         count += len(text)
 
-        print(title)
-        print(text)
+        callback(title, text)
 
-        # This is just for the logging
+        # logging
         if i % (100 - 1) == 0 and i != 0:
             if intermediary_time is None:
                 intermediary_time = time.time()
@@ -41,20 +37,6 @@ def process_wiki_to_ngram(input_filename, output_ngram_filename, depth):
                 intermediary_time = new_time
             
             logging.info(f'Saved {i+1} articles containing {count} text.')
-                
 
+    logging.info('Finished process_wiki_data(). It took {0:.2f} s to execute.'.format(round(time.time() - start, 2)))
 
-    # with open(output_ngram_filename, 'w') as out:
-    #     logging.info(
-    #         'Finished process_wiki_to_text(). It took {0:.2f} s to execute.'.format(round(time.time() - start, 2)))
-
-
-if __name__ == '__main__':
-    INPUT_FILENAME = "jawiki-latest-pages-articles.xml.bz2"
-    JA_WIKI_LATEST_URL = "https://dumps.wikimedia.org/jawiki/latest/jawiki-latest-pages-articles.xml.bz2"
-    OUTPUT_FILENAME = "2gram.json"
-    
-    if not os.path.isfile(INPUT_FILENAME):
-        wget.download(JA_WIKI_LATEST_URL)
-
-    process_wiki_to_ngram(INPUT_FILENAME, OUTPUT_FILENAME, 3)
